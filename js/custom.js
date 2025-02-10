2596b35a09076ffef65613a03d5bce0b52b49e1f
kseniadidenko81 <ksenia.vd81@gmail.com>
2025-02-10 20:33:15 +0200

    commit site

---
+++ js/custom.js
@@ -0,0 +1,182 @@
+$(function () {
+  // AOS ANIMATION
+  AOS.init({
+    disable: "mobile",
+    duration: 800,
+    anchorPlacement: "center-bottom",
+  });
+
+  // SCROLLED MENU
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.getElementById("navbar");
+
+    window.addEventListener("scroll", function () {
+      if (window.scrollY > 5) {
+        navbar.classList.add("scrolled");
+      } else {
+        navbar.classList.remove("scrolled");
+      }
+    });
+  });
+
+  // SLIDER
+  const slider = document.querySelector(".slider");
+  const prevBtn = document.querySelector(".prev-btn");
+  const nextBtn = document.querySelector(".next-btn");
+  const slides = document.querySelectorAll(".slide");
+
+  let currentIndex = 0;
+
+  function updateSlider() {
+    const slideWidth = slides[0].offsetWidth + 20;
+    slider.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+    updateArrowColors();
+  }
+
+  function updateArrowColors() {
+    if (currentIndex === 0) {
+      prevBtn.style.backgroundColor = "rgb(204 204 204 / 0%)";
+      prevBtn.style.cursor = "not-allowed";
+    } else {
+      prevBtn.style.backgroundColor = "rgb(0 0 0 / 3%)";
+      prevBtn.style.cursor = "pointer";
+    }
+
+    if (currentIndex >= slides.length - visibleSlides()) {
+      nextBtn.style.backgroundColor = "rgb(204 204 204 / 0%)";
+      nextBtn.style.cursor = "not-allowed";
+    } else {
+      nextBtn.style.backgroundColor = "rgb(0 0 0 / 3%)";
+      nextBtn.style.cursor = "pointer";
+    }
+  }
+
+  nextBtn.addEventListener("click", () => {
+    if (currentIndex < slides.length - visibleSlides()) {
+      currentIndex++;
+      updateSlider();
+    }
+  });
+
+  prevBtn.addEventListener("click", () => {
+    if (currentIndex > 0) {
+      currentIndex--;
+      updateSlider();
+    }
+  });
+
+  function visibleSlides() {
+    const screenWidth = window.innerWidth;
+    if (screenWidth >= 1280) return 5;
+    if (screenWidth >= 1024) return 4;
+    if (screenWidth >= 768) return 3;
+    if (screenWidth >= 480) return 2;
+    return 1;
+  }
+
+  window.addEventListener("resize", () => {
+    updateSlider();
+  });
+
+  window.addEventListener("load", () => {
+    updateSlider();
+  });
+
+  // FORM (INPUT + BTN)
+  $(function () {
+    $("input").on("input", function () {
+      var $input = $(this);
+      var $label = $input.siblings("label");
+
+      if ($input.val()) {
+        $label.addClass("hidden-label");
+      } else {
+        $label.removeClass("hidden-label");
+      }
+    });
+  });
+
+  $(function () {
+    $("#contact-form").on("submit", function (event) {
+      event.preventDefault();
+
+      $("#response-message")
+        .fadeIn(500)
+        .text("Thank you! We will contact you soon.");
+
+      $("#submit-btn").html(
+        '<span class="check-icon"></span> <span class="text-button">Sent</span>'
+      );
+
+      setTimeout(function () {
+        $("#response-message").fadeOut(500);
+
+        $("#submit-btn").html("Send");
+      }, 15000);
+
+      $(this).trigger("reset");
+    });
+  });
+
+  // BACK TO TOP
+  $(function () {
+    $(window).on("scroll", function () {
+      toggleBackToTopBtn();
+    });
+
+    function toggleBackToTopBtn() {
+      const $backToTopBtn = $("#backToTopBtn");
+      if ($(window).scrollTop() > 200) {
+        $backToTopBtn.fadeIn();
+      } else {
+        $backToTopBtn.fadeOut();
+      }
+    }
+
+    $("#backToTopBtn").on("click", function () {
+      $("html, body").animate({ scrollTop: 0 }, "smooth");
+    });
+  });
+});
+
+//  Go to page
+// $(function () {
+//   $(".navbar-nav .nav-link").on("click", function (event) {
+//     event.preventDefault();
+
+//     var targetPage = $(this).attr("href");
+
+//     if (targetPage !== "#") {
+//       window.location.href = targetPage;
+//     }
+//   });
+// });
+
+// ANCHOR
+// document
+//   .querySelector("#scroll-to-footer .smoothScroll")
+//   .addEventListener("click", function (event) {
+//     event.preventDefault();
+
+//     let targetId = this.getAttribute("data-target");
+
+//     if (!targetId || targetId === "#") return;
+
+//     let targetSection = document.querySelector(targetId);
+
+//     if (targetSection) {
+//       let navbarHeight = 49;
+//       let targetPosition =
+//         targetSection.getBoundingClientRect().top +
+//         window.scrollY -
+//         navbarHeight;
+
+//       window.scrollTo({
+//         top: targetPosition,
+//         behavior: "smooth",
+//       });
+//     } else {
+//       console.error("Target section not found:", targetId);
+//     }
+//   });
